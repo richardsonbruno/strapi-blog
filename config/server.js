eba41172fdda9c92f76d2ee290778bd3220f0170
richardsonbruno <richardsonbrc@gmail.com>
2021-05-25 08:55:01 -0300

    initial blog commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9bb2b53ccff5de7b75304779bd8bd05e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5e8a4bfb-b433-4304-8254-b22a16007daa'
+};
